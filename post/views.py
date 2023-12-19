7e5380590ed7f2ba8170481a26c5c8fee77ac1b0
aamirusmangani <aamirusmangani@gmail.com>
2023-12-19 19:41:44 +0530

    Created home function in views and render home.html, and configure static and media folder in settings.py file

--- post/views.py
+++ post/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
 
 # Create your views here.
+
+def home(request):
+    latest = Post.objects.order_by('-pub_date')[0:3]
+    return render(request, 'home.html', {'latest': latest})
